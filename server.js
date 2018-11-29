145d3536c7c0626b056ca9fa9cd44a379d666ec3
Eric Scott <scottzillaster@gmail.com>
2018-11-29 09:30:38 -0600

    fixed minor issues, so now I don't have to fix it each time

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const path = require("path")
-const express = require("./express")
-const bodyParser = require("./body-parser")
+const chalk = require("chalk")
+const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 
 const PORT = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
-app.use("*", (res, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
